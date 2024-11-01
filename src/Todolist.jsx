10cc99273362b4e188d42e280474e83c80099e6f
arthurpmf <arthurpinheiromfontes@gmail.com>
2024-11-01 16:52:50 -0300

    alteracao

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Todolist.css';
-import Icone from './assets/icone.png';
+import Icone from './assets/icon.png';
 
 function Todolist(){
 
